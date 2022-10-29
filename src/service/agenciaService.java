401654e1bb64cf68ad3f50b54c17a381aba59480
Cibele de Oliveira Cebola <107854253+lecibe@users.noreply.github.com>
2022-10-29 20:39:15 -0300

    Update agenciaService.java

--- src/service/agenciaService.java
+++ src/service/agenciaService.java
@@ -3,9 +3,9 @@ package service;
 import java.util.List;
 
 public interface agenciaService {
-    List<Agencia> list(String name);
+    List<Agencia> list(String nome);
     Agencia save(Agencia agencia);
-    Agencia getById(Long id);
-    Agencia update(Long id, Agencia agencia);
-    void delete(Long id);
+    Agencia getById(Integer id);
+    Agencia update(Integer id, Agencia agencia);
+    void delete(Integer id);;
 }
